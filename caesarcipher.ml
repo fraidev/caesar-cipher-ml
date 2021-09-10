29ac5f7f0ec841b7d1a6277fd21e7fb7e0a65510
Felipe Cardozo <fraifelipe@gmail.com>
2021-09-10 00:50:48 -0300

    yay

---
+++ caesarcipher.ml
@@ -0,0 +1,36 @@
+
+
+let alphabet = ['a';'b';'c';'d';'e';'f';'g';'h';'i';'j';'k';
+  'l';'m';'n';'o';'p';'q';'r';'s';'t';'u';'v';'w';'x';'y';'z';];;
+
+let modulus a b = (((a) mod b + b) mod b);;
+
+let rec find x lst =
+    match lst with
+    | [] -> raise (Failure "Not Found")
+    | h :: t -> if x = h then 0 else 1 + find x t
+
+let findAlphabetIndex input = find input alphabet;;
+
+type typeEncrypt =
+    | Sum
+    | Subtract
+
+let shift input shiftCount encrypt = 
+  let operation = 
+    match encrypt with 
+    | Sum -> (function x -> x + shiftCount)
+    | Subtract -> (function x -> x - shiftCount) in
+  String.lowercase_ascii input
+    |> String.map (function letter -> List.nth alphabet (modulus (operation (findAlphabetIndex letter)) 26));;
+          
+let encrypt input =
+    shift input 3 Sum;;
+
+let decrypt input =
+    shift input 3 Subtract;;
+
+Printf.printf "%s \n" (encrypt "ABC");;
+
+Printf.printf "%s \n" (decrypt "ABC");;
+

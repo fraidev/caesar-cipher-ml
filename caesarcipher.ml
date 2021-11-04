ad4cacf6b30d849455386a71a03ae04a96298072
Felipe Cardozo <fraifelipe@gmail.com>
2021-11-04 11:16:41 -0300

    alphabet in other file

--- caesarcipher.ml
+++ caesarcipher.ml
@@ -1,36 +1,38 @@
-
-
-let alphabet = ['a';'b';'c';'d';'e';'f';'g';'h';'i';'j';'k';
-  'l';'m';'n';'o';'p';'q';'r';'s';'t';'u';'v';'w';'x';'y';'z';];;
-
-let modulus a b = (((a) mod b + b) mod b);;
+let modulus a b = ((a mod b) + b) mod b
 
 let rec find x lst =
-    match lst with
-    | [] -> raise (Failure "Not Found")
-    | h :: t -> if x = h then 0 else 1 + find x t
+  match lst with
+  | [] -> raise (Failure "Not Found")
+  | h :: t ->
+    if x = h then
+      0
+    else
+      1 + find x t
 
-let findAlphabetIndex input = find input alphabet;;
+let findAlphabetIndex input = find input Alphabet.alphabet
 
 type typeEncrypt =
-    | Sum
-    | Subtract
+  | Sum
+  | Subtract
 
-let shift input shiftCount encrypt = 
-  let operation = 
-    match encrypt with 
-    | Sum -> (function x -> x + shiftCount)
-    | Subtract -> (function x -> x - shiftCount) in
+let shift input shiftCount encrypt =
+  let operation =
+    match encrypt with
+    | Sum -> (
+      function
+      | x -> x + shiftCount)
+    | Subtract -> (
+      function
+      | x -> x - shiftCount)
+  in
   String.lowercase_ascii input
-    |> String.map (function letter -> List.nth alphabet (modulus (operation (findAlphabetIndex letter)) 26));;
-          
-let encrypt input =
-    shift input 3 Sum;;
+  |> String.map (function letter ->
+         List.nth Alphabet.alphabet (modulus (operation (findAlphabetIndex letter)) 26))
 
-let decrypt input =
-    shift input 3 Subtract;;
+let encrypt input = shift input 3 Sum
+
+let decrypt input =   shift input 3 Subtract;;
 
 Printf.printf "%s \n" (encrypt "ABC");;
 
-Printf.printf "%s \n" (decrypt "ABC");;
-
+Printf.printf "%s \n" (decrypt "ABC")
